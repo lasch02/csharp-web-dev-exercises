2b33e3b5ca7bafd939fe200b63db63d7a2594191
Linda Schasch <lasch02@sbcglobal.net>
2023-03-28 09:48:33 -0500

    unit testing branch updated - chapter 6 exercises

--- CarTests/CarTests.cs
+++ CarTests/CarTests.cs
@@ -14,11 +14,11 @@ public class CarTests
         test_car = new Car("Toyota", "Prius", 10, 50);
     }
     //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitHub account)
-        [TestMethod]
-        public void EmptyTest() 
-        {
-            Assert.AreEqual(10, 10, .001);
-        }
+    [TestMethod]
+    public void EmptyTest() 
+    {
+        Assert.AreEqual(10, 10, .001);
+    }
 
     //TODO: constructor sets gasTankLevel properly
     [TestMethod]
@@ -29,7 +29,33 @@ public class CarTests
     }
 
     //TODO: gasTankLevel is accurate after driving within tank range
+    [TestMethod]
+    public void TestGasTank()
+    {
+        test_car.Drive(50);
+        Assert.AreEqual(9, test_car.GasTankLevel, .001);
+    }
     //TODO: gasTankLevel is accurate after attempting to drive past tank range
+    [TestMethod]
+
+    public void TestGasTankAfterExceedingTankRange()
+    {
+        test_car.Drive(501);
+        Assert.AreEqual(0, test_car.GasTankLevel, .001);
+    }
     //TODO: can't have more gas than tank size, expect an exception
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentOutOfRangeException))]
+
+    public void TestGasOverfillException()
+    {
+        test_car.AddGas(5);
+        Assert.Fail("Shouldn't get here, car cannot have more gas in tank than the size of the tank");
+    }
+
+
+
+
+
 }
 
